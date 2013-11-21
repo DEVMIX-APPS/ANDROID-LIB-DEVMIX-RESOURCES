45d3c23bde25bb018b77d7cc4dee6feaf6568106
ALAN ECHER <alan.echer@gmail.com>
2013-11-21 15:42:22 -0200

    AnimationsUtils

---
+++ DEVMIX RESOURCES/src/com/orasystems/libs/resources/AnimationsUtils.java
@@ -0,0 +1,89 @@
+package com.orasystems.libs.resources;
+
+import android.view.animation.Animation;
+
+import com.googlecode.androidannotations.annotations.EBean;
+import com.googlecode.androidannotations.annotations.res.AnimationRes;
+
+@EBean
+public class AnimationsUtils {
+
+	@AnimationRes
+	Animation appear;
+	
+	@AnimationRes
+	Animation fadeIn;
+	
+	@AnimationRes
+	Animation fadeOut;
+	
+	@AnimationRes
+	Animation growFromBottom;
+	
+	@AnimationRes
+	Animation growFromBottomLeftToTopRight;
+	
+	@AnimationRes
+	Animation growFromBottomRightToTopLeft;
+	
+	@AnimationRes
+	Animation growFromTop;
+	
+	@AnimationRes
+	Animation growFromTopLeftToBottomRight;
+	 
+	@AnimationRes
+	Animation growFromTopRightToBottomLeft;
+	
+	@AnimationRes
+	Animation pulse;
+	
+	@AnimationRes
+	Animation pushDownIn;
+	
+	@AnimationRes
+	Animation pushDownOut;
+	
+	@AnimationRes
+	Animation pushLeftIn;
+	
+	@AnimationRes
+	Animation pushLeftOut;
+	
+	@AnimationRes
+	Animation pushRightIn;
+	
+	@AnimationRes
+	Animation pushRightOut;
+	
+	@AnimationRes 
+	Animation pushUpIn;
+	
+	@AnimationRes
+	Animation pushUpOut;
+	
+	@AnimationRes
+	Animation rail;
+	
+	@AnimationRes
+	Animation rotation;
+	
+	@AnimationRes
+	Animation shrinkFromBottom;
+	
+	@AnimationRes
+	Animation shrinkFromBottomRightToTopLeft;
+	
+	@AnimationRes
+	Animation shrinkFromBottomLeftToTopRight;
+	
+	@AnimationRes
+	Animation shrinkFromTopLeftToBottomRight;
+	
+	@AnimationRes
+	Animation shrinkFromTopRightToBottomLeft;
+	
+	@AnimationRes
+	Animation ShrinkFromTop;
+	
+}
